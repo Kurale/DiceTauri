d292a475babec71ffe1125c87f9424dfc2a3a948
Nikola Kurale <nikolayka2011@gmail.com>
2025-09-08 10:32:33 +0300

    add
    add

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 fn main() {
-    dicetaurinpm_lib::run()
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
